dced36620c395db3bcdc829d99329bc81722ef2d
Endresk <60625342+Endresk@users.noreply.github.com>
2024-03-01 12:01:20 +0400

    solutions

--- block_1/comprehensions/task_1/implementation.py
+++ block_1/comprehensions/task_1/implementation.py
@@ -1,11 +1,2 @@
 def flatten_list(matrix: list) -> list:
-    """
-    Преобразует матрицу (список списков) в линейный список.
-
-    Args:
-         matrix: список, элементами которого являются списки
-
-    Returns:
-        линейный список
-    """
-    raise NotImplementedError
+    return sum(matrix, [])
