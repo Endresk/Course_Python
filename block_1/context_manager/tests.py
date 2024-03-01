106c9967972ad61f41ae181df38498eca311fb91
Endresk <60625342+Endresk@users.noreply.github.com>
2024-03-01 11:38:37 +0400

    solutions

---
+++ block_1/context_manager/tests.py
@@ -0,0 +1,26 @@
+
+class File():
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+        self.file = None
+
+    def __enter__(self):
+        self.file = open(self.filename, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.file.close()
+
+
+def file_log():
+    with File("one_lesson/log.txt", "r") as file:
+        for f in file:
+            if "LINK12" in f:
+                yield f
+
+
+if __name__ == '__main__':
+    for i in file_log():
+        print(i)
+
