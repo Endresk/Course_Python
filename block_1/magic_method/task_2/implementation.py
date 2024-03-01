f59f49c13604c211d2048f88b9fa43d0650d9162
Endresk <60625342+Endresk@users.noreply.github.com>
2024-03-01 09:10:36 +0400

    solutions

--- block_1/magic_method/task_2/implementation.py
+++ block_1/magic_method/task_2/implementation.py
@@ -1,8 +1,22 @@
 class MathClock:
     def __init__(self):
-        pass
+        self.hours = 0
+        self.minutes = 0
 
     def get_time(self):
-        return 0
+        return f"{self.hours:02d}:{self.minutes:02d}"
+
+    def __add__(self, minutes):
+        self.minutes += minutes
+        self.hours += self.minutes // 60
+        self.minutes %= 60
+
+        return self
+
+    def __mul__(self, hours):
+        self.hours += hours
+        self.hours %= 24
+        return self
+
 
 
